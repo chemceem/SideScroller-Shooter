afca02de0f95087e84ff3f8bd205a9292384c0a0
chemceem <chemceem@gmail.com>
2016-01-28 00:28:44 -0500

    Added background image. Created a scrolling effect

---
+++ Assets/_Scripts/CityController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CityController : MonoBehaviour {
+
+
+	//Private Variable
+	private Transform _transform;
+	private Vector2 _currentPosition;
+
+	//public variables
+	public float speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+		this._transform = gameObject.GetComponent<Transform>();
+		this.Reset ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this._currentPosition = this._transform.position;
+		this._currentPosition -= new Vector2 (speed, 0);
+		this._transform.position = this._currentPosition;
+		Debug.Log ("position" + this._currentPosition.x);
+
+		if (this._currentPosition.x <= -598) {
+			Reset ();
+		}
+	}
+
+	void Reset() {
+		this._transform.position = new Vector2 (600f, 0);
+	}
+		
+}
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Assignment 1 - COMP305 - W2016
 
 2D side scroller game using Unity.
 
-Background image obtained from http://opengameart.org. Open license
+Background image obtained from http://opengameart.org. 
